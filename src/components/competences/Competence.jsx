ee4d392668b718024ded34d3e4a20f4c0d9dd06d
nick-here <zuraniewski.karol@gmail.com>
2022-01-29 19:10:57 +0100

    name as a ReactNode

--- src/components/competences/Competence.jsx
+++ src/components/competences/Competence.jsx
@@ -4,14 +4,14 @@ import React from 'react';
  * Competence consisting of icon and title
  * @param {object} props
  * @param {string} props.icon - image path to the icon
- * @param {string} props.name - name of the competence
+ * @param {React.ReactNode} props.children - name of the competence
  */
-export default function Competence({ icon, name }) {
+export default function Competence({ icon, children }) {
 	return (
 		<div className='Competence'>
 			<img src={icon} alt='competence' className='Competence__icon' />
 
-			<h2 className='Competence__name'>{name}</h2>
+			<h2 className='Competence__name'>{children}</h2>
 		</div>
 	);
 }
