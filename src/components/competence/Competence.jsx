2c720cb26f90ddca09d0e4be5a9316f9a047542d
nick-here <zuraniewski.karol@gmail.com>
2022-01-29 21:07:45 +0100

    directory renamed, icon supporting require type

--- src/components/competence/Competence.jsx
+++ src/components/competence/Competence.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 /**
  * Competence consisting of icon and title
  * @param {object} props
- * @param {string} props.icon - image path to the icon
+ * @param {string | typeof import("*.svg")} props.icon - image path to the icon
  * @param {'top'|'right'|'bottom'|'left'} props.align - whether the name should be positioned on top, right, bottom or left
  * @param {React.ReactNode} props.children - name of the competence
  */
@@ -12,7 +12,11 @@ export default function Competence({ icon, align = 'right', children }) {
 		<div className='Competence'>
 			<div className={`Competence__content Competence__content--${align}`}>
 				<div className='Competence__icon'>
-					<img src={icon} alt='competence' />
+					<img
+						// @ts-ignore
+						src={icon}
+						alt='competence'
+					/>
 				</div>
 
 				<h2 className='Competence__name'>{children}</h2>
