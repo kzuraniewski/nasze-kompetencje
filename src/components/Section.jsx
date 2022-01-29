61b809c6e9130bd3995c0b830b3284f15136c9f9
nick-here <zuraniewski.karol@gmail.com>
2022-01-29 17:50:31 +0100

    Revert "section custom attributes"
    This reverts commit a90e1b68449e24e7a824ddd07f5abacaaaa2aee9.

--- src/components/Section.jsx
+++ src/components/Section.jsx
@@ -2,19 +2,17 @@ import React from 'react';
 
 /**
  * Section with a title and optional subtitle, rendering children below
- * @param {object} props
+ * @param {object} props 
  * @param {string} props.title - title of the section
  * @param {string} [props.subtitle = ''] - subtitle of the section
  * @param {string} [props.children = []] - composition
  */
-export default function Section({ title, subtitle = '', children, ...other }) {
-	return (
-		<section className='Section' {...other}>
-			<h1 className='Section__title'>{title}</h1>
+export default function Section({ title, subtitle = '', children }) {
+	return <section className="Section">
+		<h1 className="Section__title">{title}</h1>
 
-			{subtitle.length && <div className='Section__subtitle'>{subtitle}</div>}
+		{subtitle.length && <div className="Section__subtitle">{subtitle}</div>}
 
-			{children}
-		</section>
-	);
+		{children}
+	</section>
 }
