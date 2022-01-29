d2e0d86e7d58ec1d9437b9973003a909c6fb971b
nick-here <zuraniewski.karol@gmail.com>
2022-01-29 20:20:06 +0100

    competence aligns applied in jsx

--- src/components/competences/Competence.jsx
+++ src/components/competences/Competence.jsx
@@ -4,12 +4,13 @@ import React from 'react';
  * Competence consisting of icon and title
  * @param {object} props
  * @param {string} props.icon - image path to the icon
+ * @param {'top'|'right'|'bottom'|'left'} props.align - whether the name should be positioned on top, right, bottom or left
  * @param {React.ReactNode} props.children - name of the competence
  */
-export default function Competence({ icon, children }) {
+export default function Competence({ icon, align = 'right', children }) {
 	return (
 		<div className='Competence'>
-			<div className='Competence__content'>
+			<div className={`Competence__content Competence__content--${align}`}>
 				<div className='Competence__icon'>
 					<img src={icon} alt='competence' />
 				</div>
