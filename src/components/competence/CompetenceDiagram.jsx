8d459ab1175538bbc9e52b8ae657c1dd5ca27372
nick-here <zuraniewski.karol@gmail.com>
2022-01-30 21:04:29 +0100

    CompetenceDiagram working

--- src/components/competence/CompetenceDiagram.jsx
+++ src/components/competence/CompetenceDiagram.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import competences from '../competences';
 import Diagram from '../Diagram';
 import Competence from './Competence';
+import diagramImg from '../../assets/diagram.svg';
 
 /**
  * Creates a diagram with competences collecting their content from a module
@@ -25,7 +26,13 @@ export default function CompetenceDiagram() {
 
 	return (
 		<div className='CompetenceDiagram'>
-			<Diagram>
+			<div className='CompetenceDiagram__text'>Poznaj nasze kompetencje</div>
+
+			<div className='CompetenceDiagram__circle' />
+
+			<img className='CompetenceDiagram__image' src={diagramImg} alt='Diagram' />
+
+			<Diagram radius={270}>
 				{competences.map(({ icon, name }, index) => (
 					<Competence key={index} icon={icon} align={getCompetenceAlign(index)}>
 						{name}
